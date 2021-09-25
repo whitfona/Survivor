ca714733d68f0bc94f0a84fc676e0829accffd1d
Nick <nicholas@wealthyplanet.com>
2021-09-25 18:27:32 -0400

    add functionality to sumbit weekly questions

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav>
-      <NavLink activeClassName="link-active" exact to='/'>Home</NavLink>
-      <NavLink activeClassName="link-active" to='/mc-results'>Tribe Score</NavLink>
-      <NavLink activeClassName="link-active" to='/advantage-results'>Advantage</NavLink>
-      <NavLink activeClassName="link-active" to='/weeklys-results'>Weeklys</NavLink>
-      <NavLink activeClassName="link-active" exact to='/'>Survivors</NavLink>
+      <div>
+        <NavLink activeClassName="link-active" exact to='/'>Home</NavLink>
+        <NavLink activeClassName="link-active" to='/mc-results'>Tribe Score</NavLink>
+        <NavLink activeClassName="link-active" to='/advantage-results'>Advantage</NavLink>
+        <NavLink activeClassName="link-active" to='/weeklys-results'>Weeklys</NavLink>
+        <NavLink activeClassName="link-active" exact to='/'>Survivors</NavLink>
+      </div>
+      <div>
+        <Link className="btn-primary mx-0" to='/signup'>Sign Up!</Link>
+      </div>
     </nav>
   )
 }
