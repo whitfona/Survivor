e4e69ea2dbd77af939fc851e10f9d862309b040f
Nick <nicholas@wealthyplanet.com>
2021-09-26 21:31:35 -0400

    Changed styles for weekly submission form and added week useContext

--- frontend/src/Components/UserContext.js
+++ frontend/src/Components/UserContext.js
@@ -6,8 +6,9 @@ export const UserContext = createContext(null);
 export const UserProvider = ({children}) => {
 
   const [user, setUser] = useState(null);
+  const [week, setWeek] = useState(null);
 
-  const value = useMemo(() => ({user, setUser}), [user, setUser])
+  const value = useMemo(() => ({user, setUser, week, setWeek}), [user, setUser, week, setWeek])
 
   return (
     <UserContext.Provider value={value}>
