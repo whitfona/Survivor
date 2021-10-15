727f7354230db97770218de61240ebeb10846d64
Nick <nicholas@wealthyplanet.com>
2021-10-15 15:07:33 -0400

    added Procfile

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-const e = require('express');
 const bcrypt = require('bcrypt');
 
 const app = express();
@@ -15,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://survivor.whitforddesign.ca/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', false);
